2063c718df454bbdccf410de8b61674471f4c39c
Jose Tone <developer.tony17@gmail.com>
2023-01-10 13:46:00 +0100

    Initial Commit

---
+++ PulseButton/PulseButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseButtonApp.swift
+//  PulseButton
+//
+//  Created by José Tony on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PulseButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
